3f9d8e025e68c4fa0b44b9e626f3ca8546534710
Eduardo <picis_9716@hotmail.com>
2023-09-28 11:52:43 -0500

    FAMILC S.A.C v.2.0

--- resources/views/components/navegador-cliente.blade.php
+++ resources/views/components/navegador-cliente.blade.php
@@ -6,16 +6,8 @@
                 <nav>
                     <ul class="menu-item">
                         <li class="item"><a href="{{ route('home') }}">Inicio</a></li>
-                        <li class="item"><a href="{{ route('cart.index') }}">Mi carrito<svg
-                                    xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart"
-                                    width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#00b341"
-                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                                    <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                                    <path d="M17 17h-11v-14h-2" />
-                                    <path d="M6 5l14 1l-1 7h-13" />
-                                </svg></a></li>
+                        <li class="item"><a href="{{ route('cart.index') }}">Mi carrito(<span
+                                    class="boton-con-color contador-carrito"></span>)</a></li>
                         <li class="item"><a href="{{ route('busqueda.search') }}">Buscar Productos</a></li>
                         <li class="item"><a href="{{ route('product.sublimacion') }}">Sublimaciones</a></li>
                         <li class="item"><a href="{{ route('empresa.index') }}">La Empresa</a></li>
@@ -52,15 +44,14 @@
                     </label>
 
                     <a href="{{ route('cart.index') }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart"
-                            width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#00b341"
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-bag"
+                            width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#673de6"
                             fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                            <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                            <path d="M17 17h-11v-14h-2" />
-                            <path d="M6 5l14 1l-1 7h-13" />
-                        </svg>(<span class="boton-sin-color contador-carrito"></span>)
+                            <path
+                                d="M6.331 8h11.339a2 2 0 0 1 1.977 2.304l-1.255 8.152a3 3 0 0 1 -2.966 2.544h-6.852a3 3 0 0 1 -2.965 -2.544l-1.255 -8.152a2 2 0 0 1 1.977 -2.304z" />
+                            <path d="M9 11v-5a3 3 0 0 1 6 0v5" />
+                        </svg>(<span class="boton-con-color contador-carrito"></span>)
                     </a>
 
                     <div class="caja-img">
